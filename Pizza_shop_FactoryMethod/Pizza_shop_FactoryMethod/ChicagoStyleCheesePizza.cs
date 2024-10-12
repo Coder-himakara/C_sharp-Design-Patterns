9ff4d307e14ae7dde086d03a6f4b2aeb2162d333
Pasindu Himakara <146456186+Coder-himakara@users.noreply.github.com>
2024-10-12 10:29:09 +0530

    Product classes created-Pizza
    Pizza interface
    Two other styled pizzas

---
+++ Pizza_shop_FactoryMethod/Pizza_shop_FactoryMethod/ChicagoStyleCheesePizza.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Pizza_shop_FactoryMethod
+{
+    public class ChicagoStyleCheesePizza : Pizza
+    {
+        public ChicagoStyleCheesePizza() {
+            name = "Chicago Style Deep Dish Cheese Pizza";
+            dough = "Extra Thick Crust Dough";
+            sauce = "Plum Tomato Sauce";
+            toppings.Add("Shredded Mozzarella Cheese");
+        }
+        void  cut()
+        {
+            Console.WriteLine("Cutting the pizza into square slices");
+        }
+    }
+}
---
+++ Pizza_shop_FactoryMethod/Pizza_shop_FactoryMethod/Pizza.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza_shop_FactoryMethod
+{
+    public abstract class Pizza
+    {
+        protected string name;
+        protected string dough;
+        protected string sauce;
+        protected ArrayList toppings = new ArrayList();
+        
+        void prepare()
+        {
+            Console.WriteLine("Preparing " + name);
+            Console.WriteLine("Tossing dough...");
+            Console.WriteLine("Adding sauce...");
+            Console.WriteLine("Adding toppings: ");
+            foreach (String topping in toppings)
+            {
+               Console.WriteLine("   " + topping);
+            }
+        }
+        void bake()
+        {
+          Console.WriteLine("Bake for 25 minutes at 350");
+        }
+        void cut()
+        {
+           Console.WriteLine("Cutting the pizza into diagonal slices");
+        }
+        void box()
+        {
+           Console.WriteLine("Place pizza in official PizzaStore box");
+        }
+        public String getName()
+        {
+            return name;
+        }
+    }
+}
